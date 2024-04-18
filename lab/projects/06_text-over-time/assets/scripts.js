ecd42a2d5192a4b69adc43dead068b050eedbeaa
jangj864 <jangj864@newschool.edu>
2024-04-18 00:59:55 -0400

    ㅏ

--- lab/projects/06_text-over-time/assets/scripts.js
+++ lab/projects/06_text-over-time/assets/scripts.js
@@ -1,47 +1,32 @@
-function fetchAirQualityAndWeatherData() {
+function fetchWeatherData() {
     const latitude = 40.7128; // 뉴욕의 위도
     const longitude = -74.0060; // 뉴욕의 경도
-    const airQualityURL = `https://api.open-meteo.com/v1/airquality?latitude=${latitude}&longitude=${longitude}`;
     const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=1ced56361485dba64a972cfb589add52`;
 
-    // 공기 질 데이터 가져오기
-    fetch(airQualityURL)
-        .then(response => response.json())
-        .then(data => {
-            const airQualityData = {
-                ozone: data.pm25, // 초미세먼지(PM2.5) 데이터를 사용
-                co: data.pm10 // 미세먼지(PM10) 데이터를 사용
-            };
-            updatePageWithAirQualityData(airQualityData);
-        })
-        .catch(error => console.error('에어퀼리티 데이터 가져오는 중 오류 발생:', error));
-    
     // 날씨 데이터 가져오기
     fetch(weatherURL)
         .then(response => response.json())
         .then(data => {
             const weatherData = {
                 temperature: data.main.temp, // 온도
-                humidity: data.main.humidity // 상대습도
+                humidity: data.main.humidity, // 상대습도
+                windSpeed: data.wind.speed, // 바람 속도
+                precipitation: data.rain ? data.rain['1h'] : 0 // 강수량 (1시간 동안), 없으면 0
             };
             updatePageWithWeatherData(weatherData);
         })
         .catch(error => console.error('날씨 데이터 가져오는 중 오류 발생:', error));
 }
 
-// 페이지에 공기 질 데이터를 업데이트하는 함수
-function updatePageWithAirQualityData(data) {
-    document.getElementById('ozone').textContent = `${data.ozone} ppb`;
-    document.getElementById('co').textContent = `${data.co} μg/m³`; 
-}
-
 // 페이지에 날씨 데이터를 업데이트하는 함수
 function updatePageWithWeatherData(data) {
     document.getElementById('temperature').textContent = `${data.temperature}°C`;
     document.getElementById('humidity').textContent = `${data.humidity}%`;
+    document.getElementById('wind_speed').textContent = `${data.windSpeed} m/s`;
+    document.getElementById('precipitation').textContent = `${data.precipitation} mm`;
 }
 
-// 문서가 로드되면 공기 질 데이터와 날씨 데이터를 가져와서 페이지 업데이트
+// 문서가 로드되면 날씨 데이터를 가져와서 페이지 업데이트
 document.addEventListener('DOMContentLoaded', function() {
-    fetchAirQualityAndWeatherData();
+    fetchWeatherData();
 });
